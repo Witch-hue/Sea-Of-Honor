5e11ca7a2417c2643317cd371db2bd2d43cbe100
Witch <59118604+Witch-hue@users.noreply.github.com>
2020-02-13 02:15:30 -0600

    Add files via upload

---
+++ boss.js
@@ -0,0 +1,61 @@
+module.exports = {
+	// Entering the dungeon
+	DungeonInfo : [
+		
+		{zone: 3020, string: 'Entering the dungeon <font color="#56B4E9">Sea of Honor</font>'}
+	],
+	// Sea of Honor
+	SOH_BOSS_1 : [  // Sesil
+        {id: 101, msg: "Fire Ball"},
+        {id: 102, msg: "Lighting Stuck"},
+        {id: 103, msg: "Dark Lighting"},
+        {id: 104, msg: "Ice Storm"},
+				{id: 105, msg: "Escaping Bomb"},
+        {id: 106, msg: "Dark Lighting (projectile)"},
+        {id: 201, msg: "Mana Charge"},
+        {id: 202, msg: "Back Dash"}
+	],
+	SOH_BOSS_2 : [ // Keid
+        {id: 101, msg: "人类 男 祭师_惩罚 光"},
+        {id: 102, msg: "人类 男 祭师_惩罚 帷幕"},
+        {id: 103, msg: "人类 男 祭师_治愈的 风"},
+        {id: 104, msg: "人类 男 祭师_恢复场发射体"},
+        {id: 105, msg: "人类 男 祭师_毁灭"},
+        {id: 106, msg: "人类 男 祭师_毁灭 发射体"},
+        {id: 107, msg: "人类 男 祭师_毁灭 发射体2"}
+	],
+	SOH_BOSS_3 : [  // Ironfang Dixie
+        {id: 101, msg: "해적단장_combo1_노말"},
+        {id: 102, msg: "해적단장_combo1R_공용"},
+        {id: 103, msg: "해적단장_combo2_노말"},
+        {id: 104, msg: "해적단장_combo2R_공용"},
+        {id: 105, msg: "해적단장_combo3_노말"},
+        {id: 106, msg: "해적단장_combo3R_공용"},
+        {id: 107, msg: "해적단장_combo4_노말"},
+        {id: 108, msg: "해적단장_blastslash_노말"},
+        {id: 109, msg: "해적단장_sideslash_노말"},
+        {id: 120, msg: "해적단장_차지4_SelfBuffOn_공용"},
+        {id: 121, msg: "해적단장_차지1_SelfBuffOn_공용"},
+        {id: 122, msg: "해적단장_차지2_SelfBuffOn_공용"},
+        {id: 123, msg: "해적단장_차지3_SelfBuffOn_공용"},
+        {id: 124, msg: "해적단장_근폭_combo1_노말"},
+        {id: 125, msg: "해적단장_원폭_WhirlwindStart_노말"},
+        {id: 126, msg: "해적단장_큰근폭_blastSlashShot_노말"},
+        {id: 127, msg: "해적단장_DownBlow_노말"},
+        {id: 128, msg: "해적단장_SoulStrike_노말"},
+        {id: 129, msg: "해적단장_ThrowAxe_노말"},
+        {id: 131, msg: "해적단장_도끼회수_ShockingShoutStart_공용"},
+        {id: 132, msg: "해적단장_모드체인지_defenceStart_공용"},
+        {id: 133, msg: "해적단장_도끼점프_DownBlow_공용"},
+        {id: 134, msg: "해적단장_큰원폭_WhirlwindStart_노말"},
+        {id: 135, msg: "해적단장_도끼신호_ShockingShoutStart_공용"},
+        {id: 136, msg: "위치알려주는발사체"},
+        {id: 137, msg: "In -> Out"},
+        {id: 138, msg: "In -> Out (Projectile)"},
+        {id: 139, msg: "Out -> In"},
+        {id: 201, msg: "등장연출"},
+        {id: 202, msg: "defence 3초"},
+        {id: 203, msg: "defence 10초"},
+        {id: 204, msg: "패턴변경"}
+	]
+}
