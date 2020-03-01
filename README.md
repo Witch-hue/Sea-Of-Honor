a073b3ca08761506c16675610a2bf82d657c05a4
Witch <59118604+Witch-hue@users.noreply.github.com>
2020-03-01 12:22:32 -0600

    Update
    Fix Errors, Removed flowers (eye sore)

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
-# Sea Of Honor Dungon Guide Patch 90.2
-WARNING: This is still a work in progress, the guide is currently useable- but there will be proxy errors. These can be ignored. and will be fixed later. Requires more testing.
+Sea Of Honor dungeon Guide Patch 90.2
+
 
 ======
 
 Notes:
+
 Changed /8 Proxy commands from "guide" to "soh" so it does not have issues with other commonly used Dungeon guides.
 
+Translated from ZC's Guide.
 
-
-# SOH Dungeon Guide
-
+SOH Dungeon Guide
 Original Guide: https://github.com/tera-mod/TERA-Guide from ZC.
 
-------
 Commands
-
 
 /8 soh | Toggles On / Off Guide
 
 /8 soh stream | Toggles Stream Mode
 
-/8 soh party | Toggles Party Mode (Sends guide in party)
+/8 soh st | Toggles Stream Mode
--- boss.js
+++ boss.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// Entering the dungeon
 	DungeonInfo : [
-
-		{zone: 3020, string: 'Welcome too <font color="#56B4E9">Sea of Honor</font>'}
+		{zone: 3020, string: 'Welcome to <font color="#56B4E9">Sea of Honor</font>'},
 	],
+
 	// 金麟號 (Sailing Instance)
 	SOH_BOSS_1 : [
 		{id: 104, msg: 'Suction (Dodge)'}
@@ -15,25 +15,22 @@ module.exports = {
 		{id: 108, msg: 'Front Stun'},
 		{id: 128, msg: 'Upper cut (Knock up)'},
 		{id: 129, msg: 'Hammer Toss ~ Skull'},
-	//  {id: 131, msg: "毒圈射线131"},
 		{id: 135, msg: "Puddles Inc (Jump)"},
-	//  {id: 203, msg: "(防御10秒)"},
-		{id: 204, msg: "30%"},
+		{id: 204, msg: "30% Warning"},
 		{id: 137, msg: 'Outward Pluse'},
-		{id: 139, msg: 'Succ'},
+		{id: 139, msg: 'Inward Succ'},  //Lol the work Suck is censored...
 		{id: 133, msg: 'Secondary Aggro Jump -> Outward Donuts'},
-		{id: 127, msg: 'Jump -> ', TIP: ["In Safe", "Out Safe"]},
+		{id: 127, msg: 'Jump ->', TIP: ["In Safe", "Out Safe"]},
 
-		{id: 121, msg: ''},         // '前砸(外) | 转圈(内)'(绿)_来吧  +No.1 buff
-		{id: 122, msg: ''},         // '转圈(内) | 前砸(外)'(紫)_滚开  +No.1 buff
-	//  {id: 124, msg: '前砸'},     // 第一击 / 第二击
-	//  {id: 125, msg: '转圈'},     // 第一击 / 第二击
-
-		{id: 123, msg: ''},         // '蓄力前砸 | ↓(外)↓'(绿)_爆炸  +No.2 buff
-		{id: 120, msg: ''},         // '蓄力转圈 | ↑(内)↑'(紫)_深渊  +No.2 buff
-	//  {id: 126, msg: '大前砸'},  // 第三击
-	//  {id: 134, msg: '大转圈'},  // 第三击
+		{id: 121, msg: ''},         // Smash Forward (Out)| Turn (Inside) (Green)_Go In  +No.1 buff
+		{id: 122, msg: ''},         // Circle (Inside) | Smashing(Outside) (Purple)_Get Out  +No.1 buff
+	//  {id: 124, msg: 'Hammer'},     // 1st Strike, 2nd Strike
+	//  {id: 125, msg: 'Circle'},     // 1st Strike, 2nd Strike
+		{id: 123, msg: ''},         // Charge Hammer | ↓(Out)↓ (Green)_Explosion  +No.2 buff
+		{id: 120, msg: ''},         // Charged Circle | ↑(In)↑ (Purple)_Abyss  +No.2 buff
+	//  {id: 126, msg: '大前砸'},  // 3rd Strike
+	//  {id: 134, msg: '大转圈'},  // 3rd Strike
 	],
-	//           121+120=221 122+120=222           121+123=224 122+123=225
-	SOH_TipMsg : ["Out - In - In", "In - Out - In","Dodge!", "Out - In - Out", "In - Out - Out"]
+	//           121+120=241 122+120=242           121+123=244 122+123=245
+	SOH_TipMsg : ["Out - In - In", "In - Out - In","Dodge!", "Out - In - Out", "In - Out - Out"],
 }
